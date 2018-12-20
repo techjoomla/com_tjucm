675667a3e0feaaaf1230258694338c6def45afd4
Vaibhav S <vaibhav_s@tekditechnologies.com>
2018-12-20 14:29:50 +0530

    Issue #138313 fix: Item detail page >> does not displaying file download option for subform file field

--- src/components/com_tjucm/site/views/item/tmpl/default.php
+++ src/components/com_tjucm/site/views/item/tmpl/default.php
@@ -9,9 +9,20 @@
 
 // No direct access
 defined('_JEXEC') or die;
+$app = JFactory::getApplication();
 $user = JFactory::getUser();
 JLoader::import('components.com_tjfields.helpers.tjfields', JPATH_SITE);
 $TjfieldsHelper = new TjfieldsHelper;
+
+// Get Field table
+$fieldTableData = new stdClass;
+JTable::addIncludePath(JPATH_ROOT . '/administrator/components/com_tjfields/tables');
+$fieldTableData->tjFieldFieldTable = JTable::getInstance('field', 'TjfieldsTable');
+
+// Get Field value table
+$fieldValueTableData = new stdClass;
+JTable::addIncludePath(JPATH_ADMINISTRATOR . '/components/com_tjfields/tables');
+$fieldValueTableData->fields_value_table = JTable::getInstance('Fieldsvalue', 'TjfieldsTable');
 
 if ($this->form_extra)
 {
@@ -33,13 +44,16 @@ $fieldSets = $this->form_extra->getFieldsets();
 				{
 					if ($field->value)
 					{
+						// Get field value id to get the media URL
+						$fieldValueTableData->fields_value_table->load(array('value' => $field->value));
+						$extraParamArray = array();
+						$extraParamArray['id'] = $fieldValueTableData->fields_value_table->id;
 						?>
 						<div class="form-group">
-							<?php echo $field->label; ?>
-							<div class="col-sm-10">
+							<div class="col-sm-2 col-xs-12"><?php echo $field->label; ?>:</div>
+							<div class="col-sm-2">
 								<?php
-								$tjFieldHelper = new TjfieldsHelper;
-								$mediaLink = $tjFieldHelper->getMediaUrl($field->value);
+								$mediaLink = $TjfieldsHelper->getMediaUrl($field->value, $extraParamArray);
 								?>
 								<a href="<?php echo $mediaLink;?>"><?php echo JText::_("COM_TJFIELDS_FILE_DOWNLOAD");?></a>
 							</div>
@@ -49,23 +63,63 @@ $fieldSets = $this->form_extra->getFieldsets();
 				}
 				elseif ($field->type == 'Subform' || $field->type == 'Ucmsubform')
 				{
+					// Get Subform field data
+					$formData = $TjfieldsHelper->getFieldData($field->getAttribute('name'));
+
 					if ($field->value)
 					{
 						?>
 						<div class="form-group">
-							<?php echo $field->label; ?>
+							<div class="col-sm-2 col-xs-12"><?php echo $field->label; ?>:</div>
 							<div class="col-sm-10">
 							<?php
 								foreach ($field->value as $val)
 								{
 									foreach ($val as $name => $value)
 									{
-										// TODO : SubForm rendering
-										$html = '<div class="form-group">';
-											$html .= '<div class="col-sm-10"> ' . htmlspecialchars($value, ENT_COMPAT, 'UTF-8') . '</div>';
-										$html .= '</div>';
+										// Get the field data by field name to check the field type
+										$fieldTableData->tjFieldFieldTable->load(array('name' => $name));
 
-										echo  $html;
+										if ($fieldTableData->tjFieldFieldTable->label){?>
+										<div class="col-sm-2 col-xs-12"><?php echo $fieldTableData->tjFieldFieldTable->label; ?>:</div>
+										<?php } ?>
+										<div class="col-sm-10">
+										<?php
+										// If field type is file
+										if ($fieldTableData->tjFieldFieldTable->type == 'file')
+										{
+											// Get the field value id & subform file field id to get the media URL
+											$fieldValueTableData->fields_value_table->load(array('content_id' => $app->input->get('id', '', 'INT'), 'field_id' => $formData->id));
+											$extraParamArray = array();
+											$extraParamArray['id'] = $fieldValueTableData->fields_value_table->id;
+											$extraParamArray['subFormFileFieldId'] = $fieldTableData->tjFieldFieldTable->id;
+											$mediaLink = $TjfieldsHelper->getMediaUrl($value, $extraParamArray);
+											?>
+											<a href="<?php echo $mediaLink;?>"><?php echo JText::_("COM_TJFIELDS_FILE_DOWNLOAD");?></a>
+											<?php
+										}
+										// If field type is checkbox
+										elseif ($fieldTableData->tjFieldFieldTable->type == 'checkbox')
+										{
+											if ($value)
+											{
+												$checked = ($value == 1) ? ' checked="checked"' : '';
+												?>
+												<input type="checkbox" disabled="disabled" value="1" <?php echo $checked;?> />
+												<?php
+											}
+										}
+										else
+										{
+											$html = '<div class="form-group">';
+											$html .= '<div class="col-sm-10"> ' . htmlspecialchars($value, ENT_COMPAT, 'UTF-8') . '</div>';
+											$html .= '</div>';
+
+											echo  $html;
+										}
+										?>
+										</div>
+										<?php
 									}
 
 									echo '<hr>';
@@ -82,15 +136,10 @@ $fieldSets = $this->form_extra->getFieldsets();
 					{
 						?>
 						<div class="form-group">
-							<?php echo $field->label; ?>
-							<div class="col-sm-10">
+							<div class="col-sm-2 col-xs-12"><?php echo $field->label; ?>:</div>
+							<div class="col-sm-2">
 							<?php
-								$checked = "";
-
-								if ($field->value = 1)
-								{
-									$checked = ' checked="checked"';
-								}
+								$checked = ($field->value == 1) ? ' checked="checked"' : '';
 								?>
 								<input type="checkbox" disabled="disabled" value="1" <?php echo $checked;?> />
 							</div>
@@ -104,8 +153,8 @@ $fieldSets = $this->form_extra->getFieldsets();
 					{
 						?>
 						<div class="form-group">
-							<?php echo $field->label; ?>
-							<div class="col-sm-10 form-control">
+							<div class="col-sm-2 col-xs-12"><?php echo $field->label; ?>:</div>
+							<div class="col-sm-2">
 								<?php
 								if (is_array($field->value))
 								{
@@ -143,7 +192,6 @@ else
 ?>
 <div class="form-group">
 <?php
-$app = JFactory::getApplication();
 $itemid     = $app->input->getInt('Itemid', 0);
 
 if (($user->authorise('core.type.edititem', 'com_tjucm.type.' . $this->ucmTypeId)) || ($user->authorise('core.type.editownitem', 'com_tjucm.type.' . $this->ucmTypeId) && JFactory::getUser()->id == $this->item->created_by))
