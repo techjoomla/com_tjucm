f1567005d1e12b7d8229ffc2ceac0f672920a130
“ankush_m” <ankush_m@techjoomla.com>
2019-02-26 16:32:31 +0530

    Task #141143 feat: Remove extra code from installation script

--- src/components/com_tjucm/script.php
+++ src/components/com_tjucm/script.php
@@ -1,10 +1,9 @@
 <?php
 /**
- * @version    SVN: <svn_id>
- * @package    Com_Tjucm
- * @author     Techjoomla <extensions@techjoomla.com>
- * @copyright  Copyright (c) 2009-2017 TechJoomla. All rights reserved.
- * @license    GNU General Public License version 2 or later.
+ * @package    TJ-UCM
+ * @author     TechJoomla <extensions@techjoomla.com>
+ * @copyright  Copyright (c) 2009-2018 TechJoomla. All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
 
 defined('_JEXEC') or die;
