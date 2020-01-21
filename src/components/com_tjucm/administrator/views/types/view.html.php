e2212a114005b7cd145158866dc2af191f318a55
Aishwarya <aishwarya_g@gmail.com>
2020-01-21 12:09:19 +0530

    Bug#277 fix: Backend>>The title of form view of Types is not appropriate.

--- src/components/com_tjucm/administrator/views/types/view.html.php
+++ src/components/com_tjucm/administrator/views/types/view.html.php
@@ -67,11 +67,8 @@ class TjucmViewTypes extends JViewLegacy
 	{
 		$state = $this->get('State');
 		$canDo = TjucmHelper::getActions();
-		$input = JFactory::getApplication()->input;
-		$client          = $input->get('client', '', 'STRING');
-		$component_title =  JText::_('COM_TJUCM_COMPONENT');
-
-				JToolBarHelper::title($component_title .": ". JText::_('COM_TJUCM_TITLE_TYPES'), 'list');
+		$component_title = JText::_('COM_TJUCM_COMPONENT');
+		JToolBarHelper::title($component_title . ": " . JText::_('COM_TJUCM_TITLE_TYPES'), 'list');
 
 		// Check if the form exists before showing the add/edit buttons
 		$formPath = JPATH_COMPONENT_ADMINISTRATOR . '/views/type';
