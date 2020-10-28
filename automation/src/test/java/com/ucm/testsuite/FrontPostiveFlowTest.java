af350b41f79f92038446cf4a662588e0922105db
Priyanka S <priyanka_s@tekditechnologies.com>
2020-10-28 18:29:02 +0530

    added negative flow

--- automation/src/test/java/com/ucm/testsuite/FrontPostiveFlowTest.java
+++ automation/src/test/java/com/ucm/testsuite/FrontPostiveFlowTest.java
@@ -6,11 +6,13 @@ import com.ucm.pageobjects.FrontPostiveFlowPage;
 public class FrontPostiveFlowTest extends BaseClass{
 	@Test(dataProvider = "frontpostiveflow", dataProviderClass = DataProviderClass.class)
 	public void FrontPostiveFlow(String testdesc,String fnf, String nf, String ve,String vd,String eu, String ays, String ui,String cl,String sv1, String vl,String al) throws Exception {
-		FrontPostiveFlowPage frontpostive = new FrontPostiveFlowPage(driver);
+		FrontPostiveFlowPage front = new FrontPostiveFlowPage(driver);
 		
-		
+		if (testdesc.equals("negative")) {
+			front.NagativeFlow(fnf,nf,ve,vd,eu,ays,ui,cl,sv1,vl,al);
+		}		
 		if (testdesc.equals("validdata")) {
-			frontpostive.PostiveFlow(fnf,nf,ve,vd,eu,ays,ui,cl,sv1,vl,al);
+			front.PostiveFlow(fnf,nf,ve,vd,eu,ays,ui,cl,sv1,vl,al);
 		}
 	}
 }
